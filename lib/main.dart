3a4edf00e41057e11aa639347ac652a381343040
CodeNameAkshay <akshaym.ec18@nsut.ac.in>
2020-05-14 00:36:38 +0530

    homepage done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -10,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Cofista',
       theme: ThemeData(
         primarySwatch: Colors.red,
@@ -31,6 +33,104 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container());
+    return Scaffold(
+        backgroundColor: Color(0xFF333333),
+        body: SafeArea(
+          child: Column(children: [
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                          color: Color(0xFFEB5757),
+                          child: Center(
+                            child: Text("CAFE HOUSE",
+                                style: GoogleFonts.racingSansOne(fontSize: 30)),
+                          )),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+                flex: 6,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10.0),
+                  child: Container(
+                    child: Image.asset("assets/images/Rectangle 49.png",
+                        fit: BoxFit.fill),
+                  ),
+                )),
+            Expanded(
+                flex: 1,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                          child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                        child: RaisedButton(
+                          color: Color(0xFFEB5757),
+                          shape: StadiumBorder(),
+                          padding: EdgeInsets.symmetric(vertical: 20),
+                          onPressed: () {
+                            HapticFeedback.vibrate();
+                          },
+                          child: Text(
+                            'SIGN IN',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      )),
+                    ),
+                  ],
+                )),
+            Expanded(
+                flex: 1,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                          child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                        child: RaisedButton(
+                          color: Color(0xFFEB5757),
+                          shape: StadiumBorder(),
+                          padding: EdgeInsets.symmetric(vertical: 20),
+                          onPressed: () {
+                            HapticFeedback.vibrate();
+                          },
+                          child: Text(
+                            'REGISTER',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      )),
+                    ),
+                  ],
+                )),
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                          child: Center(
+                        child: Text("WELCOME!",
+                            style: GoogleFonts.oregano(
+                                fontSize: 30, color: Colors.white)),
+                      )),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ]),
+        ));
   }
 }
