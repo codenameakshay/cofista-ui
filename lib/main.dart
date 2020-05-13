02654de4f004370a3fd65d13171548518eeb29f0
CodeNameAkshay <akshaym.ec18@nsut.ac.in>
2020-05-14 02:57:58 +0530

    updated readme.md

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.red,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Profile(title: 'Cofista'),
+      home: MyHomePage(title: 'Cofista'),
     );
   }
 }
